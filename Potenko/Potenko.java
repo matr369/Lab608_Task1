1afdf36bb1a176abe916f91416f51cad2809ac8c
Dmitriy Potenko <dpotenko@yandex.ru>
2016-03-08 19:48:38 +0200

    Potenko: hello world

---
+++ Potenko/Potenko.java
@@ -0,0 +1,9 @@
+﻿import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Potenko {
+    public static void main(String[] args) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+        System.out.println("Hello world      " + dateFormat.format(new Date()));
+    }
+}
