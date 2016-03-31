7ab302ce4447bdd15527c6ee3fe2737787607532
Andrey Fedchuk <andrew414414@gmail.com>
2016-03-31 17:48:19 +0300

    small refactoring

--- Kirik2/Main.java
+++
@@ -1,9 +0,0 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class Main {
-    public static void main(String[] args) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
-        System.out.println("My Baby Left Me (at " + dateFormat.format(new Date()) + ")");
-    }
-}
